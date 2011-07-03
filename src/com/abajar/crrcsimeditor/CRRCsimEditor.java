07505b40a054d6178694219090451f496869a046
unknown <hfreire@.(none)>
2011-07-03 18:13:17 +0200

    Adding tank

--- src/com/abajar/crrcsimeditor/CRRCsimEditor.java
+++ src/com/abajar/crrcsimeditor/CRRCsimEditor.java
@@ -4,23 +4,19 @@
 
 package com.abajar.crrcsimeditor;
 
+import com.microcrowd.loader.java3d.max3ds.Loader3DS;
 import com.sun.j3d.loaders.Scene;
 import com.sun.j3d.utils.universe.SimpleUniverse;
-import com.sun.j3d.utils.universe.ViewingPlatform;
 import java.awt.BorderLayout;
 import java.io.FileNotFoundException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.media.j3d.AmbientLight;
-import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
-import javax.media.j3d.DirectionalLight;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
+import javax.media.j3d.View;
 import javax.swing.JFrame;
-import javax.vecmath.Color3f;
-import javax.vecmath.Point3d;
 import javax.vecmath.Vector3f;
 import org.jdesktop.application.Application;
 import org.jdesktop.application.SingleFrameApplication;
@@ -40,14 +36,20 @@ public class CRRCsimEditor extends SingleFrameApplication {
             frame.setLayout(new BorderLayout());
             Canvas3D canvas = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
             frame.add("Center", canvas);
+
             SimpleUniverse univ = new SimpleUniverse(canvas);
-            canvas.getView().setBackClipDistance(20000);
+            View view = canvas.getView();
+            view.setBackClipDistance(20000);
+            
             univ.getViewingPlatform().setNominalViewingTransform();
+
             TransformGroup VpTG = univ.getViewingPlatform().getViewPlatformTransform();
             Transform3D Trfcamera = new Transform3D();
             Trfcamera.setTranslation(new Vector3f(50f, 20f, 20000f));
             VpTG.setTransform(Trfcamera);
-            Scene model3d = new com.microcrowd.loader.java3d.max3ds.Loader3DS().load("Tank_BRDM3_N280611.3DS");
+
+            Loader3DS loader3ds = new Loader3DS();
+            Scene model3d = loader3ds.load(".\\model\\Tank_BRDM3_N280611.3DS");
             BranchGroup scene = model3d.getSceneGroup();
             scene.compile();
             univ.addBranchGraph(scene);
